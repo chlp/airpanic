28d54cdcbd09a2916b3be9e46c8c855337699263
Alexey R <user.raa@gmail.com>
2021-06-15 13:57:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import time
 from pathlib import Path
 
 file_name = str(Path(__file__).parent.absolute()) + '/panic.txt'
-panic_number = 20
+panic_number_max = 20
+panic_number_min = int(panic_number_max * .7)
 bot_credentials = ''
 chat_id = ''
 
-prevAir = 0
+prev_air = 0
 if os.path.isfile(file_name):
     f = open(file_name, 'r')
-    prevAir = int(f.readline())
+    prev_air = int(f.readline())
     f.close()
 
 r = requests.get('https://aircms.online/php/guiapi.php?graph&fwid=14970961')
@@ -19,15 +20,15 @@ data = r.json()['data'][0][-1]
 lastTs = data[0]
 air = data[1]
 
-if int(time.time()) - lastTs < 300:
+if int(time.time()) - lastTs < 300 and panic_number_min <= air >= panic_number_max:
     f = open(file_name, 'w')
     f.write('%d' % air)
     f.close()
 
-    if prevAir < panic_number <= air:
+    if prev_air < panic_number_max <= air:
         r = requests.post('https://api.telegram.org/bot' + bot_credentials + '/sendMessage',
                           data={'chat_id': chat_id, 'text': 'air panic:' + str(air)})
 
-    if prevAir >= (panic_number * .7) > air:
+    if prev_air >= panic_number_min > air:
         r = requests.post('https://api.telegram.org/bot' + bot_credentials + '/sendMessage',
                           data={'chat_id': chat_id, 'text': 'cancel air panic:' + str(air)})
